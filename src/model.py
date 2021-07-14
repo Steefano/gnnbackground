88ff406343e9cc502b7b9001d00f2b2b5fe33c0c
Stefano Rando <ste.rando.97@gmail.com>
2021-07-14 10:08:47 +0200

    Implemented model and training.

--- src/model.py
+++ src/model.py
@@ -37,3 +37,45 @@ class Net(torch.nn.Module):
         x = self.conv_end(x, edge_index)
 
         return torch.sigmoid(x)
+
+def train(X, y, net, train_idx, val_idx, optimizer, epochs = 100, early_stopping = 20, verbose = True):
+    criterion = nn.BCELoss()
+
+    max_acc = 0
+    epochs_without_increase = 0
+    state = None
+
+    for epoch in range(1, epochs + 1):
+
+        net.train()
+        optimizer.zero_grad()
+        output = net(X)
+        loss = criterion(output.flatten()[train_idx], y[train_idx])
+        loss.backward()
+        optimizer.step()
+
+        if verbose:
+            print("Epoch " + str(epoch) + " finished. Train loss equal to " + str(loss.item()) + ".")
+
+        net.eval()
+        with torch.no_grad():
+            output = net(X)
+        max_index = torch.where(output.flatten() > 0.5, 1, 0)
+        acc = (max_index == y)[val_idx].sum().item() / len(val_idx)
+
+        if verbose:
+            print("Validation accuracy equal to " + str(acc) + ".")
+        
+        if acc > max_acc:
+            max_acc = acc
+            epochs_without_increase = 0
+            state = net.state_dict()
+        else:
+            epochs_without_increase += 1
+            if epochs_without_increase == early_stopping:
+                break
+    
+    net.load_state_dict(state)
+    net.eval()
+
+    return acc
